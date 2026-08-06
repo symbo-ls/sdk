c0aa19817edd11c455bfdf2e3867bae579ec7120
Zachary Betzen <zachary@symbols.app>
2026-08-06 15:20:39 -0600

    stop making extra projects

--- integration-tests/availability/get-user-availability.test.js
+++ integration-tests/availability/get-user-availability.test.js
@@ -1,12 +1,11 @@
 import test from 'tape'
 
-test('get user availability', async tape => {
-  const user = await global.globalSdk.getUserByEmail('nika@symbols.app')
+test('list user availability', async tape => {
+  const user = await global.globalSdk.getUserByEmail('zachary@symbols.app')
   const availability = await global.globalSdk.getService('availabilityRules').list({
     user: user.id,
     workspaceId: user.workspaces?.[0]?.id
   })
-
   tape.ok(availability, 'availability rules returned')
 })
 
